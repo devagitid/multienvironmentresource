6296e4f6b8553f461cb81a02c9f35498614715ef
devagitid <104121621+devagitid@users.noreply.github.com>
2022-08-05 19:23:11 +0530

    Removed the azure credentials

--- providers.tf
+++ providers.tf
@@ -13,10 +13,6 @@ terraform {
 provider "azurerm" {
   features {}
 
-  subscription_id = "d2626169-7b5d-41d5-b3d0-137f693c4219"
-  client_id       = "43840f06-e48c-47fb-a418-259fc299282a"
-  client_secret   = "lsd8Q~0IfbYGuXKxvZMEKslLwUHRvl004MyzNdzm"
-  tenant_id       = "5eef40ba-552a-49e3-be68-6805b66cf278"
 }
 
 provider "databricks" {
