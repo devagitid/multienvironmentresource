977dc94856f8250665d3438d1d5dc05b061c4f94
devagitid <104121621+devagitid@users.noreply.github.com>
2022-08-08 14:28:39 +0530

    creating rg

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 terraform {
-  backend azure {}
+  backend azurerm {}
 }
 
-#resource "azurerm_resource_group" "databrrg" {
-#name = "${var.environment}-rg"
-#location = var.resource_location
-#}
+resource "azurerm_resource_group" "databrrg" {
+name = "${var.environment}-rg"
+location = var.resource_location
+}
 
 #resource "azurerm_databricks_workspace" "databr" {
 #  name                = "databricks-${var.environment}"
